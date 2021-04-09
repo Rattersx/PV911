0f29c53d991cfdf94e525b0b64c6b6dcf99dd5eb
Rattersx <66379168+Rattersx@users.noreply.github.com>
2021-04-09 19:16:51 +0300

    Add files via upload

---
+++ Mockup/Mockup/Forms/AddForms/CaloriesRedactorProductAdd.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mockup.Forms.AddForms
+{
+    public partial class CaloriesRedactorProductAdd : Form
+    {
+        public CaloriesRedactorProductAdd()
+        {
+            InitializeComponent();
+            AnimateWindow.SetAnimateWindow(this);
+        }
+
+        private void CaloriesRedactorProductAdd_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            bunifuCustomLabel1.ForeColor = guna2ColorTransition1.Value;
+        }
+
+        private void guna2Button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void titleTB_TextChanged(object sender, EventArgs e)
+        {
+            if(titleTB.Text != "")
+            {
+                Animator.ShowSync(ProteinLabel);
+                Animator.ShowSync(ProteinTB);
+            }
+        }
+
+        private void ProteinTB_TextChanged(object sender, EventArgs e)
+        {
+            if (ProteinTB.Text != "")
+            {
+                Animator.ShowSync(FatLabel);
+                Animator.ShowSync(FatTB);
+            }
+        }
+
+        private void FatTB_TextChanged(object sender, EventArgs e)
+        {
+            if (FatTB.Text != "")
+            {
+                Animator.ShowSync(CarbohydrLabel);
+                Animator.ShowSync(CarbohydrTB);
+            }
+        }
+
+        private void CaloriesTB_TextChanged(object sender, EventArgs e)
+        {
+            if (CaloriesTB.Text != "")
+            {
+                Animator.ShowSync(YesRB);
+                Animator.ShowSync(NoRB);
+            }
+        }
+
+        private void CarbohydrTB_TextChanged(object sender, EventArgs e)
+        {
+            if(CarbohydrTB.Text != "")
+            {
+                Animator.ShowSync(CaloriesLabel);
+                Animator.ShowSync(CaloriesTB);
+            }
+        }
+
+        private void YesRB_CheckedChanged(object sender, EventArgs e)
+        {
+            if (NoRB.Checked == false)
+                AcceptButton.Enabled = true;
+        }
+
+        private void NoRB_CheckedChanged(object sender, EventArgs e)
+        {
+            if (YesRB.Checked == false)
+                AcceptButton.Enabled = true;
+        }
+    }
+}
